c11e42d0515a4209979690f7ef3412fe992bd884
Scott MacDonald <smacdo@live.com>
2014-09-30 11:55:17 -0700

    adding unit test dir

---
+++ UnitTests/SimpleMathTests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+    // Unit tests for my extensions to SimpleMath.h
+	TEST_CLASS(SimpleMathTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+	};
+}
